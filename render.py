4f6bda6bd7b1a93d189252a876ca46ab2cf00fd4
anders hoff <shall.be.lifted@gmail.com>
2012-09-15 01:02:05 +0200

    implemented counting matrix.

--- render.py
+++ render.py
@@ -30,6 +30,7 @@ def showP(ctx,X,Y):
 
 def paintIt(ctx,POINTS,colors,opa=0.2):
   n = len(POINTS)/3
+  print 'elements:\t{:d}'.format(n)
   ncolors = len(colors)
   for k in xrange(0,n):
     k3 = k*3
@@ -57,23 +58,66 @@ def getColors(f):
 
   return res
 
+def countGrid(G,P):
+  n = len(P)/3
+  for k in xrange(0,n):
+    k3 = k*3
+    x = int(N*P[k3+1])
+    y = int(N*P[k3+2])
+    G[x,y] += 1
+  return
+
+def paintGrid(ctx,G):
+  scale = max(G.flatten())
+  print scale
+  G = G/float(scale)
+  for i in xrange(0,N):
+    ii = float(i)/N
+    for j in xrange(0,N):
+      ig = G[i,j]
+      if ig: 
+        rgb = 1.-ig
+        ctx.set_source_rgb(rgb,rgb,rgb)
+        ctx.rectangle(ii,float(j)/N,1./N,1./N)
+        ctx.fill()
+
+  return
+
 def main():
   
-  #colors = [[0,0,0]]
-  colors = getColors('colors.gif')
+  GRID = np.zeros((N,N))
+  POINTS = []
+  for i in xrange(0,5):
+    nameroot = 'dots{:04d}'.format(i)
+    name     = '{:s}.pkl'.format(nameroot)
+    print 'reading {:s} ... '.format(name)
+    f = open(name,'rb')
+    POINTS = pkl.load(f)
+    f.close()
+    print '\tdone'
+    print 'counting ... '
+    countGrid(GRID,POINTS)
+    POINTS = []
+    print '\tdone'
   
-  print 'reading datafile ... ',
-  f = open('dots.pkl','rb')
-  POINTS = pkl.load(f)
-  f.close()
-  print 'done'
-
+  print 'drawing ... '
   sur,ctx = ctxInit()
-
-  print 'painting ... ',
-  paintIt(ctx,POINTS,colors,opa=1)
-  print 'done'
-  sur.write_to_png('./'+OUT+'.png')
+  paintGrid(ctx,GRID)
+  sur.write_to_png('./img.01.{:s}.png'.format(nameroot))
 
 if __name__ == '__main__': main()
+
+  #for i in xrange(0,5):
+    #POINTS = []
+    #nameroot = 'dots{:04d}'.format(i)
+    #name = '{:s}.pkl'.format(nameroot)
+    #print 'reading {:s} ... '.format(name)
+    #f = open(name,'rb')
+    #POINTS = pkl.load(f)
+    #f.close()
+    #print '\tdone'
+    #print 'painting ... '
+    #paintIt(ctx,POINTS,colors,0.01)
+    #print '\tdone'
+    #sur.write_to_png('./img.2.{:s}.png'.format(nameroot))
 
