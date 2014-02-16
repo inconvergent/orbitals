80d6b6c887cb364826598033b5cb3e7e43a4e6fb
anders hoff <shall.be.lifted@gmail.com>
2014-02-16 13:12:47 +0100

    refactored from orbitals_ani.

--- orbitals.py
+++ orbitals.py
@@ -1,86 +1,67 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-def main():
-  import cairo,Image
-  from operator import itemgetter
-  from time import time
-  import numpy as np
-  from scipy.sparse import csr_matrix,lil_matrix,csc_matrix
+import cairo,Image
+from operator import itemgetter
+import numpy as np
+from numpy import sin, cos, pi, arctan2, square,sqrt, logical_not, linspace, array
+from numpy.random import random, randint
+import gtk, gobject
 
-  sin         = np.sin
-  cos         = np.cos
-  pi          = np.pi
-  sqrt        = np.sqrt
-  arctan2     = np.arctan2
-  logical_not = np.logical_not
-  float       = np.float
-  int         = np.int
-  random      = np.random.random
-  zeros       = np.zeros
-  byte        = np.byte
-  
-  PI     = pi
-  PII    = PI*2.
-
-  N      = 5000
-  NUM    = 400
-  BACK   = 1.
-  OUT    = '/data/orbitals.kunstplass/show.4'
-  RAD    = 0.26
-  GRAINS = 30
-  STP    = 0.00001
-  steps  = 500000
-  MAXFS  = 200
-  ALPHA  = 0.05
-
-  def pInit(X,Y):
-    bridge = zeros(NUM)
-    bridge[:NUM/2] = np.random.normal(size=NUM/2)*0.002
-    bridge[NUM/2:] = -bridge[:NUM/2]
-    np.random.shuffle(bridge)
-    bridge[:] = 0.15 + np.cumsum(bridge[:])
-
-    theta = np.arange(NUM,dtype=np.float) / NUM * PII
-
-    for v in xrange(NUM):
-      t = theta[v]
-      X[v] = 0.5 + cos(t)*bridge[v]
-      Y[v] = 0.5 + sin(t)*bridge[v]
+## comment out to get new results on each run
+np.random.seed(1)
 
 
-  #def pInit(X,Y):
+PI = pi
+PII = PI*2.
 
-    #for i in xrange(NUM):
-      #the = random()*PII
-      #r = RAD
-      #x = r * sin(the)
-      #y = r * cos(the)
-      #X[i] = 0.5+x
-      #Y[i] = 0.5+y
+N = 1080 # size of png image
+NUM = 200 # number of nodes
+BACK = 1. # background color 
+GRAINS = 5
+STP = 0.0001 # scale motion in each iteration by this
+MAXFS = 5 # max friendships pr node
+ALPHA = 0.05 # opacity of drawn points
+ONE = 1./N
 
+FILENAME = 'res'
 
-  def ctx_init():
+RAD = 0.20 # radius of starting circle
+FARL  = 0.13 # ignore "enemies" beyond this radius
+NEARL = 0.02 # do not attempt to approach friends close than this
+
+UPDATE_NUM = 40 # dump every UPDATE_NUM iteration to file
+#UPDATE_NUM = 10 # dump every UPDATE_NUM iteration to file
+
+FRIENDSHIP_RATIO = 0.1 # probability of friendship dens
+FRIENDSHIP_INITIATE_PROB = 0.1 # probability of friendship initation attempt
+
+COLOR_PATH = 'color/dark_cyan_white_black.gif'
+
+ANGULAR_NOISE = PII/50.
+
+class Render(object):
+
+  def __init__(self):
+
+    self.__init_cairo()
+    #self.__get_colors(COLOR_PATH)
+    self.colors = [(0,0,0)]
+    self.n_colors = 1
+
+  def __init_cairo(self):
+
     sur = cairo.ImageSurface(cairo.FORMAT_ARGB32,N,N)
     ctx = cairo.Context(sur)
     ctx.scale(N,N)
     ctx.set_source_rgb(BACK,BACK,BACK)
     ctx.rectangle(0,0,1,1)
     ctx.fill()
-    return sur,ctx
 
+    self.sur = sur
+    self.ctx = ctx
 
-  def set_distances(X,Y,R,A):
-    for i in xrange(NUM):
-      dx = X[i] - X
-      dy = Y[i] - Y
-      R[i,:] = dx*dx+dy*dy
-      A[i,:] = arctan2(dy,dx)
-    sqrt(R,R)
-    return
-
-
-  def get_colors(f):
+  def __get_colors(self,f):
     scale = 1./255.
     im = Image.open(f)
     w,h = im.size
@@ -91,133 +72,153 @@ def main():
         r,g,b = rgbim.getpixel((i,j))
         res.append((r*scale,g*scale,b*scale))
 
+    np.random.shuffle(res)
+    self.colors = res
+    self.n_colors = len(res)
+
+  def step_wrap(self,*args):
+
+    res, added_new = self.step()
+
+    if not self.itt%UPDATE_NUM:
+      self.expose()
+
+      ## write frames to file:
+      #fn = 'image{:05d}.png'.format(self.num_img)
+      #self.sur.write_to_png(fn)
+      #self.num_img+=1
+      #print fn
+
     return res
 
 
-  def makeFriends(i,R,F):
-    
-    if F[i,:].sum() > MAXFS:
-      return
+  def render_connections(self,X,Y,F,A,R):
 
-    r = []
-    for j in xrange(NUM):
-      if i != j and F[j,:].sum() < MAXFS\
-        and not F[j,i]:
-          r.append((R[i,j],j))
-    if not len(r):
-      return
-    r = sorted(r, key=itemgetter(0))
+    # everything is black
+    #self.ctx.set_source_rgba(0,0,0,ALPHA)
 
-    index = len(r)-1
-    for k in xrange(len(r)):
-      if random() < 0.1:
-        index = k
-        break
-
-    # this is bad for csc, csr matrix 
-    F[i,r[index][1]] = 1
-    F[r[index][1],i] = 1
-    return
-
-  def render_connection_points(X,Y,R,A,F,ctx,colors,alpha=ALPHA):
-    def stroke(x,y):
-        ctx.rectangle(x,y,1./N,1./N)
-        ctx.fill()
-        return
-    vstroke = np.vectorize(stroke)
-
-
-    lc = len(colors)
-    t = time()
     indsx,indsy = F.nonzero()
     mask = indsx >= indsy 
     for i,j in zip(indsx[mask],indsy[mask]):
-      a = A[i,j] ; d = R[i,j]
-
-      scales = random(GRAINS)*d
+      a = A[i,j]
+      d = R[i,j]
+      scales = np.random.random(GRAINS)*d
       xp = X[i] - scales*cos(a)
       yp = Y[i] - scales*sin(a)
-      
-      c = colors[ (i*NUM+j) % lc ]
-      ctx.set_source_rgba(c[0],c[1],c[2],alpha)
+     
+      # colors. wooo!
+      r,g,b = self.colors[ (i*N+j) % self.n_colors ]
+      self.ctx.set_source_rgba(r,g,b,ALPHA)
 
-      vstroke(xp,yp)
+      for x,y in zip(xp,yp):
+        self.ctx.rectangle(x,y,ONE,ONE)
+        self.ctx.fill()
+
+def set_distances(X,Y,A,R):
+
+  for i in xrange(NUM):
+    dx = X[i] - X
+    dy = Y[i] - Y
+    R[i,:] = square(dx)+square(dy)
+    A[i,:] = arctan2(dy,dx)
+
+  sqrt(R,R)
+
+def make_friends(i,F,R):
+
+  cand_num = F.sum(axis=1)
+  
+  if cand_num[i]>=MAXFS:
     return
 
-  def run(X,Y,SX,SY,R,A,F,NEARL,FARL):
+  cand_mask = cand_num<MAXFS
+  cand_mask[i] = False
+  cand_ind = cand_mask.nonzero()[0]
 
-    t = time()
-    set_distances(X,Y,R,A)
-    t = time()
+  cand_dist = R[i,cand_ind].flatten()
+  cand_sorted_dist = cand_dist.argsort()
+  cand_ind = cand_ind[cand_sorted_dist]
+
+  cand_n = len(cand_ind)
+
+  if cand_n<1:
+    return
+
+  for k in xrange(cand_n):
+
+    if random()<FRIENDSHIP_RATIO:
+
+      j = cand_ind[k]
+      F[[i,j],[j,i]] = True
+      return
+
+def main():
+
+  render = Render()
+
+  X = np.zeros(NUM,'float')
+  Y = np.zeros(NUM,'float')
+  SX = np.zeros(NUM,'float')
+  SY = np.zeros(NUM,'float')
+  R = np.zeros((NUM,NUM),'float')
+  A = np.zeros((NUM,NUM),'float')
+  F = np.zeros((NUM,NUM),'byte')
+
+  for i in xrange(NUM):
+    the = random()*PII
+    x = RAD * sin(the)
+    y = RAD * cos(the)
+    X[i] = 0.5+x
+    Y[i] = 0.5+y
+
+  for itt in xrange(100000):
+
+    set_distances(X,Y,A,R)
     
     SX[:] = 0.
     SY[:] = 0.
     
     for i in xrange(NUM):
-      xF        = logical_not(F[i,:])
-      d         = R[i,:]
-      a         = A[i,:]
-      near      = d > NEARL
-      near[xF]  = False
-      far       = d < FARL
+      xF = logical_not(F[i,:])
+      d = R[i,:]
+      a = A[i,:]
+      near = d > NEARL
+      near[xF] = False
+      far = d < FARL
       far[near] = False
-      near[i]   = False
-      far[i]    = False
-      speed     = FARL - d[far]
-      
-      SX[near] += cos(a[near])
-      SY[near] += sin(a[near])
-      SX[far]  -= speed*cos(a[far])
-      SY[far]  -= speed*sin(a[far])
-    t = time()
+      near[i] = False
+      far[i] = False
+      speed = FARL - d[far]
+
+      noise_far_a = 0.
+      noise_near_a = 0.
+
+      ## adds noise. can be commented out
+      #noise_near_a = (1.-2.*random(near.sum()))*ANGULAR_NOISE
+      #noise_far_a = (1.-2.*random(far.sum()))*ANGULAR_NOISE
+
+      SX[near] += cos(a[near] + noise_near_a)
+      SY[near] += sin(a[near] + noise_near_a)
+      SX[far] -= speed*cos(a[far] + noise_far_a)
+      SY[far] -= speed*sin(a[far] + noise_far_a)
 
     X += SX*STP
     Y += SY*STP
-    if random()<0.1:
-      makeFriends(int(random()*NUM),R,F)
-    t = time()
 
-    return
+    i = randint(NUM)
+    if random()<FRIENDSHIP_INITIATE_PROB:
+      make_friends(i,F,R)
 
-  sur,ctx = ctx_init()
+    render.render_connections(X,Y,F,A,R)
 
-  FARL  = 0.25
-  NEARL = 0.02
-  X  = zeros(NUM, dtype=float)
-  Y  = zeros(NUM, dtype=float)
-  SX = zeros(NUM, dtype=float)
-  SY = zeros(NUM, dtype=float)
-  R  = zeros((NUM,NUM), dtype=float)
-  A  = zeros((NUM,NUM), dtype=float)
-  F  = zeros((NUM,NUM), dtype=byte)
+    if not itt%1000:
 
-  #colors = get_colors('./color/color_black.gif')
-  #colors = get_colors('./color/color_flyby2.gif')
-  colors = [(0,0,0)]
+      fn = '{:s}{:05d}.png'.format(FILENAME,itt)
+      print fn
+      render.sur.write_to_png(fn)
 
-  pInit(X,Y)
   
-  t = time()
-  for i in xrange(steps): 
-    run(X,Y,SX,SY,R,A,F,NEARL,FARL)
-    render_connection_points(X,Y,R,A,F,ctx,colors)
-    if not (i+1)%3000:
-      sur.write_to_png('{:s}.{:06d}.png'.format(OUT,i+1))
-      print i,time()-t
-      t = time()
-
-  return
 
 if __name__ == '__main__' :
-
-  if True:
-    import pstats
-    import cProfile
-    OUT = 'profile'
-    pfilename = '{:s}.profile'.format(OUT)
-    cProfile.run('main()',pfilename)
-    p = pstats.Stats(pfilename)
-    p.strip_dirs().sort_stats('cumulative').print_stats()
-  else:
-    main()
+  main()
 
